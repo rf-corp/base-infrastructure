f9e5cf133c330b50ece5fff341c1d08b240b7a0a
Guilherme Giusti <guilherme.giusti04@gmail.com>
2024-03-24 21:11:07 -0300

    oidc

--- terraform/base-github-actions.tf
+++ terraform/base-github-actions.tf
@@ -1,3 +1,11 @@
+#https://github.blog/changelog/2023-06-27-github-actions-update-on-oidc-integration-with-aws/
+resource "aws_iam_openid_connect_provider" "github_actions" {
+  client_id_list  = ["sts.amazonaws.com"]
+  thumbprint_list = ["6938fd4d98bab03faadb97b34396831e3780aea1", "1c58a3a8518e8759bf075b76b750d4f2df264fcd"]
+  url             = "https://token.actions.githubusercontent.com"
+}
+
+
 #criando a role pra definir as policys que o git hub terá para acessar os recursos AWS
 resource "aws_iam_role" "identity-provider-github" {
   name = "github-actions"
